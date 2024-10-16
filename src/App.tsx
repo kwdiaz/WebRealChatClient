8e5b0183b4a91b829b6d6592e972b312a8ce0e35
kwdiaz <kevin.diaz@kohinorec.com>
2024-10-16 13:50:41 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Chat from './pages/Chat';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
+  const [token, setToken] = useState<string>(''); // Cambia a string en lugar de string | null
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isRegistering, setIsRegistering] = useState(false); // Estado para determinar si se está registrando
+
+  const handleLogin = (token: string) => {
+    setToken(token);
+    setIsLoggedIn(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      className="App flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-green-500" // Fondo de gradiente
+    >
+      <div className="w-full h-full max-w-md p-6 bg-white bg-opacity-80 rounded-lg shadow-md flex flex-col justify-center">
+        {!isLoggedIn ? (
+          isRegistering ? (
+            <Register setIsRegistering={setIsRegistering} />
+          ) : (
+            <Login onLogin={handleLogin} setIsRegistering={setIsRegistering} />
+          )
+        ) : (
+          <Chat token={token} />
+        )}
+      </div>
     </div>
   );
 }
